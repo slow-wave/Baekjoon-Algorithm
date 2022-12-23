e4046e72cce2a99ea6afcfc9df705a64109836bd
slow-wave <42460302+slow-wave@users.noreply.github.com>
2022-12-23 15:32:43 +0900

    [Silver II] Title: 최대 힙, Time: 148 ms, Memory: 46216 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+heap = []
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    x = int(sys.stdin.readline())
+
+    if x != 0:
+        heapq.heappush(heap, (-x, x))
+    else:
+        if not heap:
+            print(0)
+
+        else:
+            print(heapq.heappop(heap)[1])
