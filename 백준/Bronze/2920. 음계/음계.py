d9d641892d19bf43fe4a2c23a799bd731292ae4d
slow-wave <42460302+slow-wave@users.noreply.github.com>
2022-12-28 23:40:42 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+import sys
+
+nums = list(map(int, sys.stdin.readline().split()))
+if nums == sorted(nums):
+    print('ascending')
+elif nums == sorted(nums, reverse=True):
+    print('descending')
+else:
+    print('mixed')
